316b03a57311c31068b5029a4a021795c3f8b899
zhusemin <semin@126.com>
2026-06-29 07:30:07 +0800

    fix: harden ngram tokenizer

--- scripts/test-ngram.mjs
+++ scripts/test-ngram.mjs
@@ -24,9 +24,7 @@ const runnableSource = source
   .replace(/new Set<string>\(/g, 'new Set(')
   .replace(/: string\[\]/g, '')
   .replace(/: string/g, '')
-  .replace(/: number/g, '')
-  .replace(/\bconst\s+(\w+):\s*string\[\]\s*=/g, 'const $1 =')
-  .replace(/\bconst\s+(\w+):\s*Set<string>\s*=/g, 'const $1 =');
+  .replace(/: number/g, '');
 
 const context = {};
 vm.createContext(context);
@@ -61,5 +59,9 @@ assert.deepEqual(ngrams('  外套  CoAt  2026  '), [
 assert.deepEqual(ngrams('ＡＢ１２ 雨衣!!', 2, 3), ['ab', 'b1', '12', '雨衣', 'ab1', 'b12']);
 assert.deepEqual(ngrams('aa aa', 1, 2), ['a', 'aa']);
 assert.deepEqual(ngrams('a b', 3, 1), []);
+assert.deepEqual(ngrams('abc', 0, 2), []);
+assert.deepEqual(ngrams('abc', 1.5, 2), []);
+assert.deepEqual(ngrams('abc', 1, Infinity), []);
+assert.deepEqual(ngrams('𠀀衣', 1, 2), ['𠀀', '衣', '𠀀衣']);
 
 console.log('PASS');
